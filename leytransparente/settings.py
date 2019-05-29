9650f6d2af4c4b7ef313749ed9305480208cddc1
JanoVilches <alejandro.vilches@sansano.usm.cl>
2019-05-29 18:11:42 -0400

    Se agrega la conexion con MongoDB

--- leytransparente/settings.py
+++ leytransparente/settings.py
@@ -73,12 +73,22 @@ WSGI_APPLICATION = 'leytransparente.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'LeyTransparente',
+    }
+}
+
 
 
 # Password validation
