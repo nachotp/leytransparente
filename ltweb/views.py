498bf8e856c9ae8c48d68654ff59fced6ec32edc
JanoVilches <alejandro.vilches@sansano.usm.cl>
2019-06-03 20:37:54 -0400

    Se agrega la base de la web, ademas de view para subir una nueva declaracion

--- ltweb/views.py
+++ ltweb/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from pymongo import *
+import json
 
 # Create your views here.
+def Home(request):
+    return render(request, 'home.html')
+
+def SubirDeclaracion(request):
+    context = {}
+
+    myclient = MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["LeyTransparente"]
+    mycol = mydb["Declaraciones"]
+
+    if request.method == 'POST':
+        print('Nombre del diputado: ', request.POST.get('fname'))
+        print('Uploaded file: ', request.FILES['uploaded_file'])
+
+        L = ''
+        contador = 0
+
+        for chunk in request.FILES['uploaded_file']:
+
+            if(contador != 1 and contador != 2):
+                L += chunk.decode(encoding='UTF-8')
+                contador += 1
+            
+            else:
+                contador += 1
+        
+        dic = json.loads(L)
+        mycol.insert(dic)
+
+
+    return render(request, 'declaracion_form.html', context)
