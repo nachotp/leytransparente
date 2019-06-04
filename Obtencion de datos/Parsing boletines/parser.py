6941ee7b301be5de1eb93dc125843dcee00d0c2f
Ignacio Tampe <thenachotp@gmail.com>
2019-06-03 20:14:25 -0400

    parsing Etiquetas y votos de por diputados

--- Obtencion de datos/Parsing boletines/parser.py
+++ Obtencion de datos/Parsing boletines/parser.py
@@ -1,33 +1,43 @@
 import pandas as pd
+import json
+import io
 
-import itertools
-import sys
-import time
-import threading
+dfboletines = pd.read_csv("tagsBoletines.csv", sep=";")
+dfcomision = pd.read_csv("VotosComision.csv", sep=";")
+dfsala = pd.read_csv("VotosSala.csv", sep=";")
 
 
-class Spinner(object):
+boletines = dfboletines['Boletin'].unique().tolist()
+materias  = dfboletines['Materia'].unique().tolist()
+print(len(materias))
+diputados = dfsala.DIPUTADO.unique().tolist()
 
-    spinner_cycle = itertools.cycle(['-', '/', '|', '\\'])
+dictputados = {}
 
-    def __init__(self):
-        self.stop_running = threading.Event()
-        self.spin_thread = threading.Thread(target=self.init_spin)
+for dip in diputados:
+    dictputados[dip] = {}
 
-    def start(self):
-        self.spin_thread.start()
+dict_boletines = dfboletines.groupby('Boletin')['Materia'].apply(list).to_dict()
 
-    def stop(self):
-        self.stop_running.set()
-        self.spin_thread.join()
+for dip,votos in dfsala.groupby('DIPUTADO')['BOLETIN', 'VOTACION']:
+    for index, row in votos.iterrows():
+        try:
+            bol_materias = dict_boletines[row["BOLETIN"]]
+            if row['VOTACION'] in ["AFIRMATIVO", "A FAVOR"]:
+                for mat in bol_materias:
+                    if mat not in dictputados[dip]:
+                        dictputados[dip][mat] = [0,0]
+                    dictputados[dip][mat][0] += 1
+            elif row['VOTACION']  == "EN CONTRA":
+                for mat in bol_materias:
+                    if mat not in dictputados[dip]:
+                        dictputados[dip][mat] = [0,0]
+                    dictputados[dip][mat][1] += 1
+        except KeyError as e:
+            pass
+            
+with io.open("boletines.json","w", encoding='utf8') as fl:
+    fl.write(json.dumps(dict_boletines, indent=2, ensure_ascii=False))
 
-    def init_spin(self):
-        while not self.stop_running.is_set():
-            sys.stdout.write(next(self.spinner_cycle))
-            sys.stdout.flush()
-            time.sleep(0.25)
-            sys.stdout.write('\b')
-
-boletines = pd.read_csv("tagsBoletines.csv", sep=";")
-comision = pd.read_csv("tagsBoletines.csv", sep=";")
-sala = pd.read_csv("tagsBoletines.csv", sep=";")
+with io.open("votos.json","w", encoding='utf8') as fl:
+    fl.write(json.dumps(dictputados, indent=2, ensure_ascii=False))
