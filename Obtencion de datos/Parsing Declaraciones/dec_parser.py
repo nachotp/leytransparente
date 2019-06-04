f8c0b258cd2babda1cd91af9187954f557d76bc8
Franco Zalavari <fzpalma@gmail.com>
2019-06-03 22:51:45 -0400

    Lectura de giros de cada parlamentario

--- Obtencion de datos/Parsing Declaraciones/dec_parser.py
+++ Obtencion de datos/Parsing Declaraciones/dec_parser.py
@@ -2,5 +2,14 @@ import pandas as pd
 import json
 import io
 
-declaraciones = open("declaraciones.txt",'r')
-data = json.load(declaraciones)
+with open('declaraciones.json', 'r', encoding="utf8") as arch:
+    declaraciones = json.load(arch)
+
+cont = 0
+for declaracion in declaraciones:
+    try:
+        acciones = declaracion['Derechos_Acciones_Chile']
+        for accion in acciones:
+            print(accion['Giro_Registrado_SII'])
+    except KeyError:
+        pass
