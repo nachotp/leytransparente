7f2bd27535a665188feba7b331160fb54a573d1d
Franco <fzpalma@gmail.com>
2019-06-05 02:05:16 -0300

    Conflicto de interes Version 1

--- Obtencion de datos/Parsing Match/query.py
+++ Obtencion de datos/Parsing Match/query.py
@@ -1,5 +1,16 @@
 from pymongo import *
 import json
+from math import pow
+
+def score(porc, cont):
+    scr = 0
+    if(cont == "SI"):
+        scr = 10000
+    if(porc <= 50):
+        scr += pow(2,(porc/17.48))
+    else:
+        scr += pow(5,(porc/17.48))
+    return scr 
 
 myclient = MongoClient("mongodb+srv://admin:leytransparente@leytransparente-m6y51.mongodb.net/test?retryWrites"
                            "=true&w=majority")
@@ -13,7 +24,15 @@ query = mycol.find(
     {"Id_Declaracion":1, "Datos_del_Declarante": 1, "Derechos_Acciones_Chile": { "$elemMatch" : { "Giro_Registrado_SII": {"$in": kwrds} } }}
 )
 
+matches = []
 for weon in query:
-    print(weon["Datos_del_Declarante"]["nombre"])
+    nombre = weon["Datos_del_Declarante"]["Apellido_Paterno"] + " " + weon["Datos_del_Declarante"]["Apellido_Materno"] + " " + weon["Datos_del_Declarante"]["nombre"]
+    idec = weon["Id_Declaracion"]
     for emp in weon["Derechos_Acciones_Chile"]:
-        print("\t {Nombre_Razon_Social} - {Cantidad_Porcentaje} % - Controla: {Tiene_Calidad_Controlador}".format(**emp))
+        porc = float(emp["Cantidad_Porcentaje"])
+        cont = emp["Tiene_Calidad_Controlador"]
+        razon = emp["Nombre_Razon_Social"]
+        scr = score(porc,cont)
+        matches.append((scr,nombre, idec,emp))
+matches.sort(reverse=True)
+print(matches)
