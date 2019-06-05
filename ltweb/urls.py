1e0fbac8722eb2a608fdf1f493853a99adf88049
Diego Viveros <diego.viveros@sansano.usm.cl>
2019-06-04 21:08:43 -0400

    Se modifica urls y views para incluir la página "Editar.html"

--- ltweb/urls.py
+++ ltweb/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.HomeView.as_view(), name="Home"),
-    path(r'Declaracion', views.SubirDeclaracionView.as_view(), name='Subir declaracion'),
-    path(r'Diputados', views.DiputadosListView.as_view(), name='Lista Diputados')
+    path("", views.HomeView.as_view(), name='Home'),
+    path(r'Declaracion/', views.SubirDeclaracionView.as_view(), name='Subir declaracion'),
+    path(r'Editar/', views.EditarDeclaracion.as_view(), name='Editar Declaracion'),
+    path(r'Diputados/', views.DiputadosListView.as_view(), name='Lista Diputados')
 ]
--- ltweb/views.py
+++ ltweb/views.py
@@ -1,20 +1,24 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.views.generic import TemplateView, DetailView
-from django.views import View
-from pymongo import *
 import json
+
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import TemplateView
+from pymongo import *
 
 
 class HomeView(TemplateView):
     template_name = "home.html"
 # Create your views here.
 
+class EditarDeclaracion(TemplateView):
+    template_name = "editar.html"
+
+
 class SubirDeclaracionView(View):
-    
     context = {}
     initial = {'key': 'value'}
-    myclient = MongoClient("mongodb+srv://admin:leytransparente@leytransparente-m6y51.mongodb.net/test?retryWrites=true&w=majority")
+    myclient = MongoClient("mongodb+srv://admin:leytransparente@leytransparente-m6y51.mongodb.net/test?retryWrites"
+                           "=true&w=majority")
     mydb = myclient["leytransparente"]
     mycol = mydb["declaraciones"]
     template_name = 'declaracion_form.html'
@@ -23,15 +27,14 @@ class SubirDeclaracionView(View):
         return render(request, self.template_name, self.context)
 
     def post(self, request, *args, **kwargs):
-        #print('Nombre del diputado: ', request.POST.get('fname'))
-        #print('Uploaded file: ', request.FILES['uploaded_file'])
-        
+        print('Nombre del diputado: ', request.POST.get('fname'))
+        print('Uploaded file: ', request.FILES['uploaded_file'])
         L = ''
         contador = 0
 
         for chunk in request.FILES['uploaded_file']:
 
-            if(contador != 1 and contador != 2):
+            if contador != 1 and contador != 2:
                 L += chunk.decode(encoding='UTF-8')
                 contador += 1
 
@@ -39,25 +42,14 @@ class SubirDeclaracionView(View):
                 contador += 1
 
         dic = json.loads(L)
-
-        query = self.mycol.find_one({"Datos_del_Declarante.nombre":dic["Datos_del_Declarante"]["nombre"], 
-                                    "Datos_del_Declarante.Apellido_Paterno":dic["Datos_del_Declarante"]["Apellido_Paterno"], 
-                                    "Datos_del_Declarante.Apellido_Materno":dic["Datos_del_Declarante"]["Apellido_Materno"]})
-        
-        if query == None: #inserta automaticamente porque no existe nadie.
-            self.mycol.insert(dic)
-
-        else: #actualiza el registri por los datos contenidos en el JSON
-            self.mycol.update({"Datos_del_Declarante.nombre":dic["Datos_del_Declarante"]["nombre"], 
-                                "Datos_del_Declarante.Apellido_Paterno":dic["Datos_del_Declarante"]["Apellido_Paterno"], 
-                                "Datos_del_Declarante.Apellido_Materno":dic["Datos_del_Declarante"]["Apellido_Materno"]},
-                                dic)
+        self.mycol.insert(dic)
 
         return redirect('Lista Diputados')
 
+
 class DiputadosListView(TemplateView):
-    
-    myclient = MongoClient("mongodb+srv://admin:leytransparente@leytransparente-m6y51.mongodb.net/test?retryWrites=true&w=majority")
+    myclient = MongoClient("mongodb+srv://admin:leytransparente@leytransparente-m6y51.mongodb.net/test?retryWrites"
+                           "=true&w=majority")
     mydb = myclient["leytransparente"]
     mycol = mydb["declaraciones"]
     template_name = 'diputados_list.html'
@@ -69,10 +61,9 @@ class DiputadosListView(TemplateView):
         data = []
         
         for par in query:
-            dic = {}
-            dic['nombres'] = par['Datos_del_Declarante']['nombre']
-            dic['apellido_paterno'] = par['Datos_del_Declarante']['Apellido_Paterno']
-            dic['apellido_materno'] = par['Datos_del_Declarante']['Apellido_Materno']
+            dic = {'nombres': par['Datos_del_Declarante']['nombre'],
+                   'apellido_paterno': par['Datos_del_Declarante']['Apellido_Paterno'],
+                   'apellido_materno': par['Datos_del_Declarante']['Apellido_Materno']}
             data.append(dic)
 
         context['diputados'] = data
