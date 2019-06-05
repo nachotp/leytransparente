20499fe0fade672a5e06bb73f912a90c0c428886
JanoVilches <alejandro.vilches@sansano.usm.cl>
2019-06-04 22:41:52 -0400

    se modifica subir declaracion para ser acorde al ingreso del campo meta y las fechas

--- ltweb/views.py
+++ ltweb/views.py
@@ -5,7 +5,6 @@ from django.views import View
 from pymongo import *
 import json
 
-
 class HomeView(TemplateView):
     template_name = "home.html"
 # Create your views here.
@@ -16,15 +15,13 @@ class SubirDeclaracionView(View):
     initial = {'key': 'value'}
     myclient = MongoClient("mongodb+srv://admin:leytransparente@leytransparente-m6y51.mongodb.net/test?retryWrites=true&w=majority")
     mydb = myclient["leytransparente"]
-    mycol = mydb["declaraciones"]
+    mycol = mydb["dipu_pruebas"]
     template_name = 'declaracion_form.html'
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, self.context)
 
     def post(self, request, *args, **kwargs):
-        #print('Nombre del diputado: ', request.POST.get('fname'))
-        #print('Uploaded file: ', request.FILES['uploaded_file'])
         
         L = ''
         contador = 0
@@ -39,19 +36,23 @@ class SubirDeclaracionView(View):
                 contador += 1
 
         dic = json.loads(L)
+        dic["Meta"] = True
 
         query = self.mycol.find_one({"Datos_del_Declarante.nombre":dic["Datos_del_Declarante"]["nombre"], 
                                     "Datos_del_Declarante.Apellido_Paterno":dic["Datos_del_Declarante"]["Apellido_Paterno"], 
-                                    "Datos_del_Declarante.Apellido_Materno":dic["Datos_del_Declarante"]["Apellido_Materno"]})
+                                    "Datos_del_Declarante.Apellido_Materno":dic["Datos_del_Declarante"]["Apellido_Materno"],
+                                    "Meta":True})
         
         if query == None: #inserta automaticamente porque no existe nadie.
             self.mycol.insert(dic)
 
         else: #actualiza el registri por los datos contenidos en el JSON
-            self.mycol.update({"Datos_del_Declarante.nombre":dic["Datos_del_Declarante"]["nombre"], 
-                                "Datos_del_Declarante.Apellido_Paterno":dic["Datos_del_Declarante"]["Apellido_Paterno"], 
-                                "Datos_del_Declarante.Apellido_Materno":dic["Datos_del_Declarante"]["Apellido_Materno"]},
-                                dic)
+            if(dic["Fecha_de_la_Declaracion"] > query["Fecha_de_la_Declaracion"]):
+                self.mycol.update({"Datos_del_Declarante.nombre":dic["Datos_del_Declarante"]["nombre"], 
+                                    "Datos_del_Declarante.Apellido_Paterno":dic["Datos_del_Declarante"]["Apellido_Paterno"], 
+                                    "Datos_del_Declarante.Apellido_Materno":dic["Datos_del_Declarante"]["Apellido_Materno"],
+                                    "Meta":True}, { "$set": {"Meta": False}})
+                self.mycol.insert(dic)
 
         return redirect('Lista Diputados')
 
@@ -59,7 +60,7 @@ class DiputadosListView(TemplateView):
     
     myclient = MongoClient("mongodb+srv://admin:leytransparente@leytransparente-m6y51.mongodb.net/test?retryWrites=true&w=majority")
     mydb = myclient["leytransparente"]
-    mycol = mydb["declaraciones"]
+    mycol = mydb["dipu_pruebas"]
     template_name = 'diputados_list.html'
 
     def get_context_data(self, **kwargs):
