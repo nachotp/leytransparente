94a99b970cb54fc64b7cc9a74e8d907485deb0e5
ChrisGC <christopher.gilbert@sansano.usm.cl>
2019-10-02 13:03:33 -0300

    login_required urls.py
    se agrega la restriccion de login para algunas urls

--- ltweb/urls.py
+++ ltweb/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required, permission_required
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="Home"),
     path(r'ley', views.SubirLeyView.as_view(), name='Subir Ley'),
-    path(r'leyes', views.LeyesListView.as_view(), name='Lista Leyes'),
+    path(r'leyes', login_required(views.LeyesListView.as_view()), name='Lista Leyes'),
     path(r'declaracion/', views.SubirDeclaracionView.as_view(), name='Subir declaracion'),
-    path(r'diputados/', views.DiputadosListView.as_view(), name='Lista Diputados'),
-    path(r'ver/<str:id>', views.ViewDeclaracion.as_view(), name='Ver Declaracion'),
+    path(r'diputados/', login_required(views.DiputadosListView.as_view()), name='Lista Diputados'),
+    path(r'ver/<str:id>', login_required(views.ViewDeclaracion.as_view()), name='Ver Declaracion'),
     path(r'conflicto/<str:ley>', views.ConflictoView.as_view(), name='Conflictos'),
-    path(r'conflictos', views.ConflictoListView.as_view(), name='Lista Conflictos'),
+    path(r'conflictos', login_required(views.ConflictoListView.as_view()), name='Lista Conflictos'),
     path(r'registro', views.RegistroView.as_view(), name='Registro de usuario'),
     path(r'login/', LoginView.as_view(), name='login'),
     path(r'logout/', LogoutView.as_view(), name='logout'),
